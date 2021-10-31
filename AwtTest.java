2a2264accd5a01db5b85b0d520a8341fc0a5e0d0
AmanCSE-1 <78953880+AmanCSE-1@users.noreply.github.com>
2021-10-31 23:08:25 +0530

    Update AwtTest.java

--- AwtTest.java
+++ AwtTest.java
@@ -4,7 +4,7 @@ import java.awt.event.*;
 public class AwtTest extends Frame implements ItemListener {
     Label lb;
     CheckboxGroup options;
-    Checkbox a1,a2;
+    Checkbox a1,a2,a3,a4;
 
     public AwtTest(){
         setTitle("AwtTest Example");
@@ -23,6 +23,11 @@ public class AwtTest extends Frame implements ItemListener {
         lb.setBounds(70,50, 160, 30);
         lb.setBackground(Color.LIGHT_GRAY);
         lb.setVisible(true);
+        
+        author = new Label();
+        author.setText("Aman Singh");
+        author.setBounds(140, 240, 200, 20);
+        author.setBackground(Color.YELLOW);
 
         Label status = new Label();
         status.setBounds(80,210,150,20);
@@ -33,6 +38,10 @@ public class AwtTest extends Frame implements ItemListener {
         a1.setBounds(80, 80, 80, 30);
         a2= new Checkbox("Chennai",options,false);
         a2.setBounds(80, 110,80, 30);
+        a3= new Checkbox("New Dehli",options,false);
+        a3.setBounds(80, 140,80, 30);
+        a4= new Checkbox("Kolkata",options,false);
+        a4.setBounds(80, 170,80, 30);
 
         a1.addItemListener(new ItemListener(){
             public void itemStateChanged(ItemEvent e) {
@@ -47,12 +56,28 @@ public class AwtTest extends Frame implements ItemListener {
                 status.setText(" Your Answer is Incorrect");
             }
         });
-
         
+        a3.addItemListener(new ItemListener(){
+            public void itemStateChanged(ItemEvent e) {
+                status.setBackground(Color.GREEN);
+                status.setText(" Your Answer is Correct");
+            }
+        });
+
+        a4.addItemListener(new ItemListener(){
+            public void itemStateChanged(ItemEvent e) {
+                status.setBackground(Color.RED);
+                status.setText(" Your Answer is Incorrect");
+            }
+        });
+
+        add(author);
         add(status);
         add(lb);
         add(a1);
         add(a2);
+        add(a3);
+        add(a4);
 
         }
 
